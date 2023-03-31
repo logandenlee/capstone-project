05fefbe1c76499014ce97fa2977c2e82a48b5d31
tasfari <89539382+tasfari@users.noreply.github.com>
2023-03-31 14:09:38 -0500

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,17 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+
+if(username) {
+
+}
+else {
+    alert("Incorrect username/password. Please try again.")
+}
+
+
+
+
+
+}
