e24938718a5d0d3419e22d364ed0bb684dd6b45a
tasfari <89539382+tasfari@users.noreply.github.com>
2023-04-19 14:11:05 -0500

    sign in/sign up page completed

--- validation.js
+++ validation.js
@@ -1,8 +1,51 @@
-const username = document.getElementById('username').trim;
-const password = document.getElementById('password').trim;
-const form = document.getElementById('form');
-const errorElement = document.getElementById('error');
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form--message");
 
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add('form__message--${type}');
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // Perform AJAX/Fetch login
+
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
+
+/*
 form.addEventListener('submit', (e) => {
     let messages = []
 
@@ -33,7 +76,7 @@ form.addEventListener('submit', (e) => {
 }
 )
 
-/*
+
 function validate() {
     
     var username=document.getElementById("username").value;
