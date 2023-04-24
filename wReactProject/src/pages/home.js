72f177864c3893c7b2afb3a9646ed38194828169
Logan Day <ldd14@students.uwf.edu>
2023-04-24 12:29:53 -0500

    just a basic message left on home screen

--- wReactProject/src/pages/home.js
+++ wReactProject/src/pages/home.js
@@ -1,16 +1,32 @@
+import profilePic from '../images/mockProfile.jpg'
+import puppyPic from '../images/puppy.jpg'
+
 export default function Home() {
     return (
         <div className="homePage">
-
+            <div className="homeTitle">
+            WELCOME TO GAME CHANGER
+            </div>
             <div className="wrapperTwo">
                 <div className="leftHome">
+                <img src={profilePic} 
+            alt="user" width="100"/>
                     <p>Admin</p>
                 </div>
                 <div className="rightHome">
-                    <p>Welcome to our Home Page! Here you will find plenty of information of our website</p>
+                    
+                    <p>Welcome to our Home Page! Here you will find plenty of information of our website. Admins will make comments on what's going on 
+                        to keep the community up-to-date on what's worth participating in!
+                    </p>
+                    <p>
+                        <p>While you're stopping by, have a puppy while we begin our event catalog!</p>
+                    </p>
+                    <img src={puppyPic} 
+            alt="user" width="300"/>
                 </div>
             </div>
         </div>
+        
     );
 }
 
